61d64196d7b7dd2eed3b16be67cc2cb4801bfa31
ke ma <make@makedeimac.local>
2018-05-23 11:08:27 +0800

    Initial Commit

---
+++ Timer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Timer
+//
+//  Created by make on 2018/5/23.
+//  Copyright © 2018年 make. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Timer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Timer
+//
+//  Created by make on 2018/5/23.
+//  Copyright © 2018年 make. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
